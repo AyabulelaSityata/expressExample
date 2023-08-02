3754d3d48241ba722ea21134c3ea1b3f1c168ebb
AyabulelaSityata <sityataayabulela@gmail.com>
2023-08-02 12:02:18 +0200

    retrieving and updating

--- index.js
+++ index.js
@@ -1,61 +1,102 @@
-const express = require('express')
-const app = express()
-const port = ++process.env.PORT || 3000
-const path = require('path')
-const db = require('./config')
+const express = require("express");
+const app = express(); // same as using http.createServer
+const port = +process.env.PORT || 3000;
+const path = require("path");
+const db = require("./config");
+const bodyParser = require("body-parser");
 // Static file: a middle way that allows us to serve the static file
 // in order to register the middle way we use app.use
 
-app.use(express.static('./static'))
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, './static/html/index.html'))  //we are sending the index.html to the user
-    // res.status(200).json(
-    //     {
-    //         msg: "You are home"
-    //     }
-    // )
-})
+app.use(express.static("./static"));
+// Home page
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, "./static/html/index.html")); //we are sending the index.html to the user
+  // res.status(200).json(
+  //     {
+  //         msg: "You are home"
+  //     }
+  // )
+});
 
-app.get('/about', (req, res) => {
-    res.status(200).json(
-        {
-            msg: "About Page"
-        }
-    )
-})
+app.get("/about", (req, res) => {
+  res.status(200).json({
+    msg: "About Page",
+  });
+});
 // app.all('*', (req, res) => {
 //     res.json({
-//         status: res.statusCode, 
+//         status: res.statusCode,
 //         msg: 'An error occurred'
 //     })
 // })
 
-get.get('/users', (req, res) => {
+// retrieving all users 02/08/2023 this is the best practice
+// app.get("/users", (req, res) => {
+//   const query = `
+//     SELECT userID, firstName, lastName 
+//     FROM Users;
+//     `;
+//   db.query(query, (err, data) => {
+//     if (err) throw err;
+//     res.json({
+//       status: res.statusCode,
+//       results: data,
+//     });
+//   });
+// });
+
+// RETRIEVING A SINGLE USER
+app.get("/user", (req, res) => {
     const query = `
-    SELECT userID, firstName, 
-    lastName, 
+      SELECT userID, firstName, lastName 
+      FROM Users;
+      `;
+    db.query(query, (err, data) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        results: data,
+      });
+    });
+  });
+
+// registering a new user
+app.post("/register", bodyParser.json(), (req, res) => {
+  const query = `
+    INSERT INTO Users  
+    SET ?;  
+`;
+  db.query(query, [req.body], (err) => {
+    if (err) throw err;
+    res.json({
+      status: res.statusCode,
+      msg: "Registration was successful.",
+    });
+  });
+});
+
+app.get('/users', (req, res) => {
+    const query = `
+    SELECT userID, firstName, lastName
     FROM Users;`
     db.query(query, (err, data) => {
-        if(!err) {
-            res.status(200).json(
+        if(err) throw err
+        res.json( 
                 {
+                    status: res.statusCode,
                     results: data
                 }
             )
-        }
-        res.status(404).json(
-            {
-                msg: 'An error occurred'
-            }
-        )
     })
 })
 
+// On a port where the server will listen from
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
 
-app.post('./createTable', (req, res) => {
+// app.post('./createTable', (req, res) => {
 
-})
+// })
